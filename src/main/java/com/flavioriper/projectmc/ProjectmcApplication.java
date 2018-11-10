086bfa1660714c90d4945f584973a691b06aa1bb
flavioriper <flavioriper_andrade@hotmail.com>
2018-11-09 23:42:08 -0200

    First Commit

---
+++ src/main/java/com/flavioriper/projectmc/ProjectmcApplication.java
@@ -0,0 +1,12 @@
+package com.flavioriper.projectmc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectmcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectmcApplication.class, args);
+	}
+}
---
+++ src/test/java/com/flavioriper/projectmc/ProjectmcApplicationTests.java
@@ -0,0 +1,16 @@
+package com.flavioriper.projectmc;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ProjectmcApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
