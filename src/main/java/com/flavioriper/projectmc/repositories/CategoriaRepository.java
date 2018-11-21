9e6ffd2b9235c0359fae8281bf22b2d413ba022d
flavioriper <flavioriper_andrade@hotmail.com>
2018-11-21 21:17:08 -0200

    Criando repository e service para Categoria

---
+++ src/main/java/com/flavioriper/projectmc/repositories/CategoriaRepository.java
@@ -0,0 +1,11 @@
+package com.flavioriper.projectmc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.flavioriper.projectmc.domain.Categoria;
+
+@Repository
+public interface CategoriaRepository extends JpaRepository<Categoria, Integer> {
+	
+}
