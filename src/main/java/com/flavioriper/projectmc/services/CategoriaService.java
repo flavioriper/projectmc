5565b2bf47f87bbd2af20fa137f58cecca4e8c16
flavioriper <flavioriper_andrade@hotmail.com>
2018-12-10 20:38:07 -0200

    Operacao DELET Categoria

--- src/main/java/com/flavioriper/projectmc/services/CategoriaService.java
+++ src/main/java/com/flavioriper/projectmc/services/CategoriaService.java
@@ -3,10 +3,12 @@ package com.flavioriper.projectmc.services;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import com.flavioriper.projectmc.domain.Categoria;
 import com.flavioriper.projectmc.repositories.CategoriaRepository;
+import com.flavioriper.projectmc.services.exceptions.DataIntegrityException;
 import com.flavioriper.projectmc.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -15,9 +17,29 @@ public class CategoriaService {
 	@Autowired
 	private CategoriaRepository repo;
 	
-	public Categoria buscar(Integer id) {
+	public Categoria find(Integer id) {
 		Optional<Categoria> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException (
 				"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
+	}
+	
+	public Categoria insert(Categoria obj) {
+		obj.setId(null);
+		return repo.save(obj);
+	}
+	
+	public Categoria update(Categoria obj) {
+		find(obj.getId());
+		return repo.save(obj);
+	}
+	
+	public void delete(Integer id) {
+		find(id);
+		try {
+			repo.deleteById(id);
+		}
+		catch(DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Não é possível deletar uma categoria que possui produtos");
+		}
 	}
 }
