6c07e600349c6dd7649987d8c87c89222115928a
flavioriper <flavioriper_andrade@hotmail.com>
2018-12-11 20:13:59 -0200

    Criado Categoria DTO e paginacao

--- src/main/java/com/flavioriper/projectmc/services/CategoriaService.java
+++ src/main/java/com/flavioriper/projectmc/services/CategoriaService.java
@@ -1,9 +1,13 @@
 package com.flavioriper.projectmc.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.flavioriper.projectmc.domain.Categoria;
@@ -41,5 +45,14 @@ public class CategoriaService {
 		catch(DataIntegrityViolationException e) {
 			throw new DataIntegrityException("Não é possível deletar uma categoria que possui produtos");
 		}
+	}
+	
+	public List<Categoria> findAll(){
+		return repo.findAll();
+	}
+	
+	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction){
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		return repo.findAll(pageRequest);
 	}
 }
