a5b873ac3ba6e72dc9beaccd1327148ddf1b485d
flavioriper <flavioriper_andrade@hotmail.com>
2018-12-18 20:49:57 -0200

    metodo de inserir cliente e correcoes menores

--- src/main/java/com/flavioriper/projectmc/services/CategoriaService.java
+++ src/main/java/com/flavioriper/projectmc/services/CategoriaService.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.flavioriper.projectmc.domain.Categoria;
+import com.flavioriper.projectmc.dto.CategoriaDTO;
 import com.flavioriper.projectmc.repositories.CategoriaRepository;
 import com.flavioriper.projectmc.services.exceptions.DataIntegrityException;
 import com.flavioriper.projectmc.services.exceptions.ObjectNotFoundException;
@@ -33,8 +34,9 @@ public class CategoriaService {
 	}
 	
 	public Categoria update(Categoria obj) {
-		find(obj.getId());
-		return repo.save(obj);
+		Categoria newObj = find(obj.getId());
+		updateData(newObj, obj);
+		return repo.save(newObj);
 	}
 	
 	public void delete(Integer id) {
@@ -54,5 +56,13 @@ public class CategoriaService {
 	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction){
 		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
 		return repo.findAll(pageRequest);
+	}
+	
+	public Categoria fromDTO(CategoriaDTO objDto) {
+		return new Categoria(objDto.getId(), objDto.getNome());
+	}
+	
+	private void updateData(Categoria newObj, Categoria obj) {
+		newObj.setNome(obj.getNome());
 	}
 }
