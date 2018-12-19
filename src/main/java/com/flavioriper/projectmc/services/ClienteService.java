5161fe8cb2bb633c203002c7348a788723896454
flavioriper <flavioriper_andrade@hotmail.com>
2018-12-19 18:44:35 -0200

    validacao customizada

--- src/main/java/com/flavioriper/projectmc/services/ClienteService.java
+++ src/main/java/com/flavioriper/projectmc/services/ClienteService.java
@@ -57,7 +57,7 @@ public class ClienteService {
 			repo.deleteById(id);
 		}
 		catch(DataIntegrityViolationException e) {
-			throw new DataIntegrityException("Não é possível deletar um cliente com entidades relacionadas");
+			throw new DataIntegrityException("Não é possível deletar um cliente com pedidos relacionadas");
 		}
 	}
 	
